ce484a394411163f0b938c50d7239c0f5427ec94
dimasik47k-ship-it <webtools.suite@mail.ru>
2026-03-26 13:13:40 +0400

    feat: добавил Vercel Speed Insights для мониторинга производительности

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { useEffect } from 'react';
 import { initTheme } from '../lib/theme';
-import '../styles/globals.css'; // 🔥 ДОБАВЬ ЭТУ СТРОКУ!
+import { SpeedInsights } from '@vercel/speed-insights/next'; // 🔥 Импортируем
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     initTheme();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights /> {/* 🔥 Добавляем компонент */}
+    </>
+  );
 }
 
 export default MyApp;
